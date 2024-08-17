713c4acc4b1dc362eb6c04a67ddab6c41fd7a956
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-08-17 19:17:13 +0530

    upload project

---
+++ index.js
@@ -0,0 +1,11 @@
+const userTab=document.querySelector("[data-userWeather]");
+const searchTab=document.querySelector("[data-searchWeather]");
+data-grantAcess
+data-searchForm
+data-searchInput
+data-cityName
+data-weatherDesc
+data-countryIcon
+data-weatherDesc
+data-weatherIcon
+data-temp
